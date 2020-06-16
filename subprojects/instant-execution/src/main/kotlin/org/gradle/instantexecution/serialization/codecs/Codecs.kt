c63800aa05133e615eef957a52a10dbdf3057388
Adam Murdoch <adam@gradle.com>
2020-06-16 14:23:14 +1000

    Fix for serialization of serialized snapshot.

--- subprojects/instant-execution/src/main/kotlin/org/gradle/instantexecution/serialization/codecs/Codecs.kt
+++ subprojects/instant-execution/src/main/kotlin/org/gradle/instantexecution/serialization/codecs/Codecs.kt
@@ -73,7 +73,6 @@ import org.gradle.internal.serialize.BaseSerializerFactory.PATH_SERIALIZER
 import org.gradle.internal.serialize.BaseSerializerFactory.SHORT_SERIALIZER
 import org.gradle.internal.serialize.BaseSerializerFactory.STRING_SERIALIZER
 import org.gradle.internal.snapshot.ValueSnapshotter
-import org.gradle.internal.snapshot.impl.IsolatedSerializedValueSnapshot
 import org.gradle.internal.state.ManagedFactoryRegistry
 import org.gradle.process.ExecOperations
 import org.gradle.process.internal.ExecActionFactory
